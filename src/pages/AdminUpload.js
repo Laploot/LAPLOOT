b14875f60179c3ad991961c01122b4f872b81507
Laploot <laploot.shop@gmail.com>
2025-10-29 01:11:59 +0530

    Update AdminUpload.js

--- src/pages/AdminUpload.js
+++ src/pages/AdminUpload.js
@@ -48,7 +48,7 @@ const AdminUpload = () => {
     });
 
     try {
-      const res = await axios.post('http://localhost:5000/api/laptops', data, {
+      const res = await axios.post('${process.env.REACT_APP_API_URL}/api/laptops', data, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
