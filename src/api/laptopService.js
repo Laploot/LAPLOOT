96cf0dfb36b7743e30f93c593433e76d5bfefe03
Laploot <laploot.shop@gmail.com>
2025-10-29 01:04:01 +0530

    Update laptopService.js
    updation of API

--- src/api/laptopService.js
+++ src/api/laptopService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'http://localhost:5000/api/laptops';
+const API = 'process.env.REACT_APP_API_URL/api/laptops';
 
 export const fetchLaptops = async () => {
   const res = await axios.get(API);
